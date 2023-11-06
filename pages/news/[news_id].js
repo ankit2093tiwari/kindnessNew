a6c778fe8b477045bfdccd69e3c82b07587c475c
ankit2093tiwari <39435198+ankit2093tiwari@users.noreply.github.com>
2023-11-06 09:27:26 +0530

    Update [news_id].js

--- pages/news/[news_id].js
+++ pages/news/[news_id].js
@@ -171,7 +171,7 @@ function NewsDetailPage({ data, Newsid }) {
 
           <meta property="og:type" content="website" />
           <meta property="og:url" content={process.env.BASE_LIVE_URL + "news/" + data?.id} />
-          <meta property="og:title" content={data?.title} />
+          <meta property="og:title" content='News Title' />
           <meta property="og:description" content={data.news_artical} />
           <meta property="og:image" content={process.env.SITE_URL + data?.media} />
 
